9b68a9a2eccf71d4ca9cfd4f24681683b9a6ae4d
jkll <452394856@qq.com>
2023-08-30 15:23:41 +0800

    1

---
+++ src/main/java/com/example/nativetest/TestController.java
@@ -0,0 +1,16 @@
+package com.example.nativetest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jkll
+ * @date 2023/8/30
+ */
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
